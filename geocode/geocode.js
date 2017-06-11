5a8e29d9f89ea47c8157b4cd84fd873cf6bc912f
Peter Palocz <peter.palocz91@gmail.com>
2017-06-11 19:14:23 +0200

    refactor to promise

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,31 +1,33 @@
 const request = require("request");
 
-function getCoords(address, callback) {
+function getCoords(address) {
   const encodedAddress = encodeURIComponent(address);
 
-  request(
-    {
-      uri: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-      json: true
-    },
-    (error, response, body) => {
-      if (error) {
-        // console.log(`error: `, error);
-        callback("Unable to connect to Google servers", null);
-      } else if (body.status === "ZERO_RESULTS") {
-        callback(`Unable to find that address`);
-      } else if (body.status === "OK") {
-        const result = {
-          address: body.results[0].formatted_address,
-          coords: {
-            longitude: body.results[0].geometry.location.lng,
-            latitude: body.results[0].geometry.location.lat
-          }
-        };
-        callback(null, result);
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        uri: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+        json: true
+      },
+      (error, response, body) => {
+        if (error) {
+          // console.log(`error: `, error);
+          reject("Unable to connect to Google servers");
+        } else if (body.status === "ZERO_RESULTS") {
+          reject(`Unable to find that address`);
+        } else if (body.status === "OK") {
+          const result = {
+            address: body.results[0].formatted_address,
+            coords: {
+              longitude: body.results[0].geometry.location.lng,
+              latitude: body.results[0].geometry.location.lat
+            }
+          };
+          resolve(result);
+        }
       }
-    }
-  );
+    );
+  });
 }
 
 module.exports.getCoords = getCoords;
