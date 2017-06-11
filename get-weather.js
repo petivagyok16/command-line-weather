f863445756866435e2b6b62da78f03809fae4d83
Peter Palocz <peter.palocz91@gmail.com>
2017-06-11 21:16:47 +0200

    refactor(app): refactor getWeather into a separate file

---
+++ get-weather.js
@@ -0,0 +1,24 @@
+const geocode = require("./geocode/geocode");
+const weather = require("./weather/weather");
+const yargs = require("./config/yargs");
+
+const getWeather = () => {
+  geocode
+    .getCoords(yargs.argv.address)
+    .then(result => {
+      console.log(JSON.stringify(result, undefined, 2));
+      weather
+        .getWeather(result.coords)
+        .then(result => {
+          console.log(JSON.stringify(result, undefined, 2));
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+module.exports.getWeather = getWeather();
