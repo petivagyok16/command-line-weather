04feb6167fcce86c9e87a4e02196ee9463cb2bba
Peter Palocz <peter.palocz91@gmail.com>
2017-06-11 19:18:11 +0200

    refactor to promise

--- weather/weather.js
+++ weather/weather.js
@@ -1,20 +1,22 @@
 const request = require("request");
 const apikey = "8496da6d689f3d7a78517b7cca5f7460";
 
-function getWeather(coords, callback) {
-  request(
-    {
-      uri: `https://api.darksky.net/forecast/${apikey}/${coords.latitude},${coords.longitude}`,
-      json: true
-    },
-    (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        callback(null, body.currently);
-      } else {
-        callback("Unable to fetch weather");
+function getWeather(coords) {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        uri: `https://api.darksky.net/forecast/${apikey}/${coords.latitude},${coords.longitude}`,
+        json: true
+      },
+      (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          resolve(body.currently);
+        } else {
+          reject("Unable to fetch weather");
+        }
       }
-    }
-  );
+    );
+  });
 }
 
 module.exports.getWeather = getWeather;
