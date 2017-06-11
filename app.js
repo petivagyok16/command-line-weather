ce437f0f8f78597aef7ac2e9b66432f39a4bd5e7
Peter Palocz <peter.palocz91@gmail.com>
2017-06-11 18:59:04 +0200

    refactor some shit lel

--- app.js
+++ app.js
@@ -13,18 +13,18 @@ const argv = yargs
   })
   .help()
   .alias("help", "h").argv;
-
 // console.log(argv);
+
 geocode.getCoords(argv.address, (error, result) => {
   if (error) {
     console.log(error);
   } else if (result) {
-    // console.log(JSON.stringify(result, undefined, 2));
+    console.log(JSON.stringify(result, undefined, 2));
     weather.getWeather(result.coords, (error, result) => {
       if (error) {
         console.log(error);
       } else {
-        console.log(result);
+        console.log(JSON.stringify(result, undefined, 2));
       }
     });
   }
