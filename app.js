af9aa2a9a95da24bbe844d1c0c7f7a2309e71293
Peter Palocz <peter.palocz91@gmail.com>
2017-06-11 16:55:12 +0200

    add geocoding & weather info

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-const request = require("request");
 const yargs = require("yargs");
+const geocode = require("./geocode/geocode");
+const weather = require("./weather/weather");
 
-const argv = yargs.options({
-  a: {
-    demand: true,
-    alias: "address",
-    describe: "Address to fetch weather for",
-    string: true
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
+// console.log(argv);
+geocode.getCoords(argv.address, (error, result) => {
+  if (error) {
+    console.log(error);
+  } else if (result) {
+    // console.log(JSON.stringify(result, undefined, 2));
+    weather.getWeather(result.coords, (error, result) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(result);
+      }
+    });
   }
 });
-
-request(
-  {
-    uri: "https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20street%20philadelphia",
-    json: true
-  },
-  (error, response, body) => {
-    if (error) {
-      console.log(`error: `, error);
-    }
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    // console.log(JSON.stringify(body, undefined, 2));
-  }
-);
