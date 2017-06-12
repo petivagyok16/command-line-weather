f130ea53c68f82e97afc6afc40408277ecc94f53
Peter Palocz <peter.palocz91@gmail.com>
2017-06-12 08:24:06 +0200

    fix(runWeather): fix paths

--- bin/runWeather.js
+++ bin/runWeather.js
@@ -3,9 +3,9 @@ const shell = require("shelljs");
 const command = process.argv.slice(2).join(" ").trim();
 
 if (command === "-h" || command === "--help") {
-  shell.exec(`node ../app.js -h`);
+  shell.exec(`node app.js -h`);
 } else if (command.includes("-a")) {
-  shell.exec(`node ../app.js '${command}'`);
+  shell.exec(`node app.js '${command}'`);
 } else {
-  shell.exec(`node ../app.js -a '${command}'`);
+  shell.exec(`node app.js -a '${command}'`);
 }
